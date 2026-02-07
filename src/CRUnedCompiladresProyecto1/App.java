d2516daceb8f364c36972a74077e51db80deba29
EGUX-a <125316285+Stolaraga@users.noreply.github.com>
2026-02-06 20:52:38 -0600

    Cambios en nombres
    Para facilitar los imports y trabajar más fácilmente que la anterior que use sólo porque me dio pereza y use nombres más cortos en los paquetes

--- src/CRUnedCompiladresProyecto1/App.java
+++ src/CRUnedCompiladresProyecto1/App.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package CRUnedCompiladresProyecto1;
 
@@ -9,5 +9,12 @@ package CRUnedCompiladresProyecto1;
  * @author Elias
  */
 public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
